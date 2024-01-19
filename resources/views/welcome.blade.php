a93ee91972af0b5a9656412cd5a6789a87d9439e
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-01-19 10:04:46 +0400

    second normal commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,76 +3,63 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="SLV" content="Сделаем лучше вместе">
-    <meta name="request" content="Заявка в администрацию">
-    <meta name="SLV" content="Сделаем лучше вместе">
-    <meta name="SLV" content="Сделаем лучше вместе">
-    <link rel="shortcut icon" href="{{asset('img/index/LOGO.svg')}}" type="image/png">
     <title>SLV</title>
+    @vite("resources/css/app.css")
 </head>
 <body>
-    {{--  HEADER  --}}
-    <header>
-        <nav class="nav-bar">
-            <div class="nav-bar-logo">
-                <img src="" alt="">
-            </div>
-            <ul class="navbar-link">
-                <li><a href="">Новости</a></li>
-                <li><a href="">Заявки</a></li>
-                <li><a href="">Контакты</a></li>
-            </ul>
-            <div class="nav-bar-btn">
-                <button>Войти</button>
-            </div>
-        </nav>
+    <header class="flex flex-row justify-between items-center px-[95px] pt-[95px]">
+        <div>
+            <img src="{{asset('img/index/LOGO.svg')}}" alt="" srcset="">
+        </div>
+        <div class="text-[#243659] text-[18px] no-underline flex gap-x-[97px]">
+            <a href="">Новости</a>
+            <a href="">Заявка</a>
+            <a href="">Контакты</a>
+        </div>
+        <div>
+            <button class="bg-[#0D3C99] rounded-md w-[207px] h-[60px] text-white text-[24px] shadow-xl duration-[0.5s] hover:scale-[1.1] hover:text-[28px]">
+                Войти
+            </button>
+        </div>
     </header>
-    {{--       MAIN        --}}
     <main>
-        <section class="slider_section">
+        <section class="slider">
 
         </section>
-        <section class="requests_section">
-            <p>Заявок: 4</p>
-            <div class="requests_container">
-                <div class="request_card">
-a
+        <section class="posts grid grid-cols-2 pl-[384px] gap-y-[150px]  ">
+            <div class="post shadow-lg w-[414px]">
+                <img src="{{asset('img/login/image 7.png')}}" class="w-full " alt="post_image">
+                <div class="post-content px-5 py-2">
+                    <p class="text-[20px] title">Плохие дороги <img src="{{asset('img/index/Done.svg')}}" alt="" srcset=""></p>
+                    <p class="genre">#дороги</p>
+                    <p class="time">11.12.2004</p>
                 </div>
-                <div class="request_card">
-b
+            </div>
+            <div class="post shadow-lg w-[414px]">
+                <img src="{{asset('img/login/image 7.png')}}" class="w-full " alt="post_image">
+                <div class="post-content px-5 py-2">
+                    <p class="text-[20px] title">Плохие дороги <img class="w-6" src="{{asset('img/index/Done.svg')}}" alt="" srcset=""></p>
+                    <p class="genre">#дороги</p>
+                    <p class="time">11.12.2004</p>
                 </div>
-                <div class="request_card">
-c
-                </div>
-                <div class="request_card">
-d
+            </div>
+            <div class="post shadow-lg w-[414px]">
+                <img src="{{asset('img/login/image 7.png')}}" class="w-full " alt="post_image">
+                <div class="post-content px-5 py-2">
+                    <p class="text-[20px] title">Плохие дороги <img src="{{asset('img/index/Done.svg')}}" alt="" srcset=""></p>
+                    <p class="genre">#дороги</p>
+                    <p class="time">11.12.2004</p>
                 </div>
             </div>
         </section>
-        <section class="send_request_section">
-            <h2>Есть проблемы в городе? Оставьте заявку!</h2>
-            <button>Оставить заявку</button>
+        <section class="hav_prob">
+
         </section>
     </main>
-    {{--     FOOTER     --}}
     <footer>
-        <div class="footer_icons">
-            <ul >
-                <li><img class="footer_icon" src="{{asset('img/index/telegram.svg')}}" alt=""></li>
-                <li><img class="footer_icon" src="{{asset('img/index/vk.svg')}}" alt=""></li>
-                <li><img class="footer_icon" src="{{asset('img/index/odnoklassniki.svg')}}" alt=""></li>
-            </ul>
-        </div>
-        <img src="{{asset('img/index/lane.svg')}}" class="lane" alt="">
-        <div class="other_info">
-            <ul>
-                <li><a href="" class="other_info_a">©️ Copyright</a></li>
-                <li class="company_rules"><a href="" class="other_info_a company_rules">Правила компании</a></li>
-                <li class="politic_conf"><a href="" class="other_info_a politic_conf">Политика конфидицальности</a></li>
-            </ul>
-        </div>
+
     </footer>
+@vite("resources/js/app.js")
 </body>
 </html>
