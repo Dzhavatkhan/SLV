9a81cbfc1b8088d5b25fc37c6706784a3bef336d
Dzhavatkhan <rp11dzhavatkhanov@gmail.com>
2024-01-27 21:42:25 +0400

    auth & reg

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -12,9 +13,15 @@ class AuthController extends Controller
         return view('auth.reg');
     }
     public function signUp(Request $request){
+        $avatar = $request->image;
+        $avatar = time().'.'.$avatar->extension();
+        $request->image->move(public_path('img/profiles'),$avatar );
+
+
         $user = new User();
         $user->name = $request->name;
         $user->password = bcrypt($request->password);
+        $user->image = $avatar;
         $user->save();
         if ($user->save()) {
             //
@@ -24,6 +31,21 @@ class AuthController extends Controller
         return view('auth.login');
     }
     public function login_proccess(LoginRequest $request){
-        dd($request->login);
+        $data = $request->validate([
+            "login" => ['string', 'required'],
+            "password" => ['required']
+        ]);
+
+    if (auth('web')->attempt($data)) {
+        return redirect(route("profile", Auth::id()));
+    }
+    if (auth('admin')->attempt($data)) {
+        return redirect(route("admin"));
+    }
+
+
+    return redirect(route('signIn'))->withErrors([
+    "login" => "Пользователь не найден, либо данные были введены неверно"
+]);
     }
 }
